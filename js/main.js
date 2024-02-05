a0aa9e05c32998e7e67ac3b0470f7be537bfd88f
ThibautFgi <thibaut.fgi@hotmail.com>
2024-02-05 16:55:34 +0100

    some button

--- js/main.js
+++ js/main.js
@@ -12,3 +12,18 @@ gitButton.addEventListener('mouseout', function() {
     // Remove the new class when the hover ends
    git.classList.remove('hover-class');
 });
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    var myPortfolioButton = document.getElementById("myPortfolioButton");
+
+    // Add a click event listener to the button
+    myPortfolioButton.addEventListener("click", function() {
+        // Define the URL you want to navigate to
+        var portfolioLink = "https://www.craftz.dog/";
+
+        // Navigate to the specified link
+        window.location.href = portfolioLink;
+    });
+});

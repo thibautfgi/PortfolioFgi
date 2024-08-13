63fd3d3d2b79e8751ac0d6155e5dc94f770374d0
ThibautFgi <thibaut.fgi@hotmail.com>
2024-08-13 15:48:57 +0200

    some progress

--- portfolio/src/app/components/home/home.component.ts
+++ portfolio/src/app/components/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SceneComponent } from "../scene/scene.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [SceneComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })

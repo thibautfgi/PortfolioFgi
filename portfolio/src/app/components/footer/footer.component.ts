e2e248a83334cc7d4f264f4d960c8aa147fb7585
ThibautFgi <thibaut.fgi@hotmail.com>
2024-08-13 15:08:21 +0200

    404 n footer n header done

--- portfolio/src/app/components/footer/footer.component.ts
+++ portfolio/src/app/components/footer/footer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
